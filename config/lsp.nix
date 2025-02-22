e80b4c1f1849db0e3b0431275bfc25316c2d5c97
barsch <iffland.lars@gmail.com>
2025-02-22 09:11:00 +0100

    enable ruff

--- config/lsp.nix
+++ config/lsp.nix
@@ -6,6 +6,7 @@
       lspSignature.enable = true;
       otter-nvim.enable = true;
       lsplines.enable = true;
+      ruff.enable = true;
       nvim-docs-view = {
         enable = true;
       };
