97aae0ed1da7b8f0a38a7cfdaa15e2449208ab5f
barsch <iffland.lars@gmail.com>
2025-02-23 10:32:26 +0100

    debugger, neotest keymaps

--- config/keys.nix
+++ config/keys.nix
@@ -5,5 +5,20 @@
       key = "<C-q>";
       action = "<CMD>q<CR>";
     }
+    {
+      mode = ["n"];
+      key = "<leader>tr";
+      action = "<CMD>lua require('neotest').run.run()<CR>";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>tf";
+      action = "<CMD>lua require('neotest').run.run(vim.fn.expand('%'))<CR>";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>td";
+      action = "<CMD>lua require('neotest').run.run({strategy = 'dap'})<CR>";
+    }
   ];
 }
